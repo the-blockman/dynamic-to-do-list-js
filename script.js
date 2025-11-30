2c4393cb5aaa1c201ad8e34280d6472ccb02bc6d
the-blockman <105639638+the-blockman@users.noreply.github.com>
2025-11-30 21:22:17 +0100

    Implement addTask function to handle task creation and removal

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", function () {
+  //select DOM elements
   const addButton = document.getElementById("add-task-btn");
+  const taskInput = document.getElementById("task-input");
+  const taskList = document.getElementById("task-list");
+
+  //create the addTask Function
+  function addTask() {
+    let taskText = taskInput.value.trim();
+    if (taskText === "") {
+      alert("enter a task");
+      return;
+    }
+
+    //create li element
+    const li = document.createElement("li");
+    li.textContent = taskText;
+
+    //create remove button
+    const removeBtn = document.createElement("button");
+    removeBtn.textContent = "Remove";
+    removeBtn.classList.add("remove-btn");
+  }
 });
